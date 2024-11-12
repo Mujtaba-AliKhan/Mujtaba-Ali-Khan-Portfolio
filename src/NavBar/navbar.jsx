36ed31592f9414fbb7aa7b99259b9cc3e46fee59
Mujtaba Ali Khan <154321048+Mujtaba-AliKhan@users.noreply.github.com>
2024-11-12 12:44:39 +0530

    Update navbar.jsx

--- src/NavBar/navbar.jsx
+++ src/NavBar/navbar.jsx
@@ -1,7 +1,14 @@
 import "./navbar.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className="navbarContainer">
       <div className="logoContainer">
@@ -9,19 +16,38 @@ const Navbar = () => {
           <img src="/makLogo.png" alt="logo" height={"80px"} />
         </Link>
       </div>
-      <div className="navItemContainer">
+      <div>
+        <button
+          className={`menuToggle ${isSidebarOpen ? "rotate" : ""}`}
+          onClick={toggleSidebar}
+        >
+          ☰
+        </button>
+      </div>
+
+      <div className={`navItemContainer ${isSidebarOpen ? "open" : ""}`}>
         <ul className="navList">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" onClick={toggleSidebar}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/aboutme">About Me</Link>
+            <Link to="/aboutme" onClick={toggleSidebar}>
+              About Me
+            </Link>
           </li>
           <li>
-            <Link to="/projectgallery">Project Gallery</Link>
+            <Link to="/projectgallery" onClick={toggleSidebar}>
+              Project Gallery
+            </Link>
           </li>
           <li>
-            <Link to="/Mujtaba Ali Khan Resume.pdf" target="blank">
+            <Link
+              to="/Mujtaba Ali Khan Resume.pdf"
+              target="blank"
+              onClick={toggleSidebar}
+            >
               Resume
             </Link>
           </li>
